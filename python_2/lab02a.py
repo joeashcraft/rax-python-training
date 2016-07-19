9e380219f026e78def5157f4d948c6d0e4c68c14
Joe Ashcraft <joe.ashcraft@rackspace.com>
2016-07-19 14:56:41 -0500

    lab02a, lab02b

--- python_2/lab02a.py
+++ python_2/lab02a.py
@@ -9,12 +9,11 @@ file = open("./Python II Data/alice_in_wonderland.dat", 'r')
 data = file.read()
 file.close()
 
-for line in data:
-    for character in line:
-        if character in 'eE':
+for character in data:
+    if character.isalpha():
+        alpha_count += 1
+        if character.lower() == 'e':
             e_count += 1
-        if character in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ':
-            alpha_count += 1
 
 print "%-27s %9d" % ("Count of alpha characters:", alpha_count)
 print "%-27s %9d" % ("Count of 'e':", e_count)
