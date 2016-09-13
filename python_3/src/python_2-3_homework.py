1557a03634adcf7f01334ded012956f8062c4ce3
student <joe.ashcraft@rackspace.com>
2016-09-13 11:22:32 -0500

    working python2-3 homework

--- python_3/src/python_2-3_homework.py
+++ python_3/src/python_2-3_homework.py
@@ -1,34 +1,42 @@
-#! env python
 """Python 3 Prereq from Python 2.
 
 In the data from Python II find, "alice_in_wonderland.dat."
-You will also find a file labeled, "words.txt." The latter file contains over 100,000 English-language words.
+You will also find a file labeled, "words.txt." The latter file contains over
+100,000 English-language words.
 
 Your job is to perform the following:
-* Create a dictionary for counting using the entries from words.txt as the keys.
+* Create a dictionary for counting using the entries from words.txt as
+the keys.
 
-* Parse the text in alice_in_wonderland.data isolating each word. This requires removing/replacing all punctuation and using the split method.
+* Parse the text in alice_in_wonderland.data isolating each word. This requires
+removing/replacing all punctuation and using the split method.
     * Make sure the words in the book are all lower case.
 
-* Find each word from the book in your dictionary and increase the count for that word by one.
-    * If a word is not found in the dictionary, place it in a list with other unfound words.
+* Find each word from the book in your diction ary and increase the count for
+that word by one.
+    * If a word is not found in the dictionary, place it in a list with other
+    unfound words.
 
-* When you have processed the entire book, determine the percentage of words in the dictionary that were used in the book,
+* When you have processed the entire book, determine the percentage of words in
+the dictionary that were used in the book,
     * and which word was used the most.
 
-* Remove the duplicates from the list of unfound words, sort it and print it. Don't try too hard to make this perfect. It won't happen!
+* Remove the duplicates from the list of unfound words, sort it and print it.
+Don't try too hard to make this perfect. It won't happen!
 """
 
-from string import punctuation, whitespace
+from string import punctuation
 
 # ? split on whitespace and punctuation
 # ? which word was used the most
 
-file = open('/Users/joe6623/Training/python/python_2/Python II Data/alice_in_wonderland.dat', 'r')
+# file = open('/Users/joe6623/Training/python/python_2/Python II Data/alice_in_wonderland.dat', 'r')
+file = open('/home/student/python-training/python_2/Python II Data/alice_in_wonderland.dat', 'r')
 bookin = file.read().lower()
 file.close()
 
-file = open('/Users/joe6623/Training/python/python_2/Python II Data/words.txt', 'r')
+# file = open('/Users/joe6623/Training/python/python_2/Python II Data/words.txt', 'r')
+file = open('/home/student/python-training/python_2/Python II Data/words.txt', 'r')
 wordsin = file.read().lower()
 file.close()
 
@@ -42,8 +50,13 @@ for word in wordsin.split():
 ##   This requires removing/replacing all punctuation and using the split method.
 ##   Make sure the words in the book are all lower case.
 book = []
-# sep = punctuation + whitespace
-# print sep
+
+for x in punctuation:
+    if x == "'":
+        bookin = bookin.replace("'", '')  # remove apostrophes
+    else:
+        bookin = bookin.replace(x, ' ')   # replace other punctuation with space
+
 for word in bookin.split():
     book.append(word)
 book_set = set(book)
@@ -60,7 +73,7 @@ for word in book_set:
         # word goes in to SET of unfound words
         unfound_words.add(word)
 
-## DEBUG
+# DEBUG
 # print type(book)
 # print type(book_set)
 # print book[0:4]
@@ -70,7 +83,7 @@ for word in book_set:
 # # print "adventures used {} times.".format(good_words['adventures'])
 # for i in range(4):
 #     print "{} is an unfound word".format(list(unfound_words)[i])
-##/DEBUG
+# /DEBUG
 
 
 ## When you have processed the entire book, determine the percentage of words in the dictionary that were used in the book
@@ -78,20 +91,17 @@ for word in book_set:
 # how many words in word list?
 print "Words in dictionary:  {}".format(len(dictionary))
 # how many good words used in book?
-count = 0
-for i in good_words.values():
-    count += i
+# len(good_words.values())
 
-print "Good words in book:  {}".format(count)
+print "Good words in book:  {}".format(len(good_words.values()))
 # add all values of good_words{}
-print "Percent of book words found in dictionary: {0:.3%}".format(float(count)/len(dictionary))
+print "Percent of book words found in dictionary: {0:.3%}".format(
+    float(len(good_words.values()) - len(unfound_words)) / len(dictionary)
+)
 
 ## and which word was used the most.
-# count_good_words = []
-# for k, v in good_words.items():
-#     count_good_words.append([v, k])
-#
-# print "{} was used the most, {} times.".format(max(count_good_words))
+sorted_good_words = sorted(zip(good_words.itervalues(), good_words.iterkeys()))
+print "{} was used the most, {} times.".format(sorted_good_words[-1][1], sorted_good_words[-1][0])
 
 ## Remove the duplicates from the list of unfound words, sort it and print it. Don't try too hard to make this perfect. It won't happen!
 print "Unfound Words:  {}".format(len(unfound_words))
